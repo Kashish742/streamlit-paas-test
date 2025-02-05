323195827eaaa9b4d59b7fff65e68cc334d9f7fc
Kashish742 <kashish9300@gmail.com>
2025-02-05 17:12:00 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,12 +1,4 @@
-import subprocess
-import sys
-
-# Try installing OpenCV if it's not installed
-try:
-    import cv2
-except ImportError:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "opencv-python"])
-    import cv2
+import cv2
 
 import numpy as np
 import os
